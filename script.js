68a68071e9c5743e06a054b5d8743009d2d1ecbd
Orjuwan7 <arjmm448@gmail.com>
2026-07-17 21:42:19 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPage2() {
+    document.getElementById('page1').classList.add('hidden');
+    document.getElementById('page2').classList.remove('hidden');
+}
+
+function showFinal() {
+    document.getElementById('page2').classList.add('hidden');
+    document.getElementById('page3').classList.remove('hidden');
+}
